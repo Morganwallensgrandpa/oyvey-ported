432842806059353a578384775b2f0022dd665b34
Morganwallensgrandpa <ashdelahoussaye@icloud.com>
2026-09-20 10:21:41 -0500

    NoFallModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/NoFallModule.java
@@ -6,7 +6,7 @@ import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 
 public class NoFallModule extends Module {
     public NoFallModule() {
-        super("NoFall", "Removes fall damage", Category.PLAYER);
+        super("AnkleInsurance", "Turns terminal velovity into a light inconvenience", Category.PLAYER);
     }
 
     @Override
